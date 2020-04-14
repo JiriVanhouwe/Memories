9d72f258ed9bf370967a050c06560e4cdbbf73ac
Jiri Vanhouwe <jiri.vanhouwe@student.hogent.be>
2020-04-14 14:02:33 +0200

    Add-memory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MemoryListComponent } from './memories/memory-list/memory-list.component';
 import { LocationComponent } from './memories/location/location.component';
+import { AddMemoryComponent } from './memories/add-memory/add-memory.component';
 
 @NgModule({
   declarations: [  //hier komen componenten die tot de module behoren
-    AppComponent, MemoryListComponent, LocationComponent
+    AppComponent, MemoryListComponent, LocationComponent, AddMemoryComponent
   ],
   imports: [
     BrowserModule, 
---
+++ src/app/memories/add-memory/add-memory.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-memory',
+  templateUrl: './add-memory.component.html',
+  styleUrls: ['./add-memory.component.css']
+})
+
+export class AddMemoryComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
