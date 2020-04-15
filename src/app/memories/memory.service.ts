512cfe003261f5471205fe9fb7228ff178abcb39
Jiri Vanhouwe <jiri.vanhouwe@student.hogent.be>
2020-04-15 14:12:55 +0200

    Toevoegen /api environment

--- src/app/memories/memory.service.ts
+++ src/app/memories/memory.service.ts
@@ -3,6 +3,7 @@ import { IMemory, Memory } from './memory';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -19,57 +20,8 @@ export class MemoryService {
   }
 
     getMemories$() : Observable<Memory[]>{
-      return this.http.get<Memory[]>(this.url)
+      return this.http.get<Memory[]>(`${environment.apiUrl}/memories/`)
         .pipe(tap(data => console.log('All: ' + JSON.stringify(data))), catchError(this.handleError));
-
-        // return this.http.get(`${environment.apiUrl}/recipes/`).pipe(
-        //   tap(console.log),
-        //   shareReplay(1),
-        //   catchError(this.handleError),
-        //   map((list: any[]): Recipe[] => list.map(Recipe.fromJSON))
-        // );
-
-
-        // return [
-        //     {
-        //       "memoryId" : 1,
-        //       "title": "Reis kaapverdië",
-        //       "subTitle": "Kuieren op de met zon overgoten witte stranden.",
-        //       "startDate": '2020-03-11T18:25:43.511Z',
-        //       "endDate": '2020-03-11T18:25:43.511Z',
-        //       "imageUrl": "assets/Sun.jpg",
-        //       "location": {
-        //         "country": "Kaapverdië",
-        //         "city": "Sal Rei"
-        //       }
-              
-        //     },
-        //     {
-        //       "memoryId" : 2,
-        //       "title": "Wandeling Etretat",
-        //      "subTitle": "Eindeloos verdwalen het groen van Etretat.",
-        //      "startDate": '2020-01-07T18:25:43.511Z',
-        //       "endDate": '2020-01-10T18:25:43.511Z',
-        //      "imageUrl": "assets/Forest.jpg",
-        //      "location": {
-        //       "country": "Frankrijk",
-        //       "city": "Etretat"
-        //     }
-        // },
-
-        //     {
-        //         "memoryId" : 3,
-        //         "title": "Diner in Amigo Amigo",
-        //        "subTitle": "Culinair genieten in Gent!",
-        //        "startDate": '2020-02-20T18:25:43.511Z',
-        //         "endDate": '2020-02-20T18:25:43.511Z',
-        //        "imageUrl": "assets/Amigo.jpg",
-        //        "location": {
-        //         "country": "België",
-        //         "city": "Gent"
-        //       }
-        //       }
-        //   ];
     }
 
     private handleError(error : HttpErrorResponse){
